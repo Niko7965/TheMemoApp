f039abf5f63ac8a8686158d56b9d957b6f8186a0
Niko7965 <54095558+Niko7965@users.noreply.github.com>
2020-01-22 10:53:25 +0100

    #Change to 1.8, making audio work

---
+++ src/main/java/MinimSound.java
@@ -0,0 +1,57 @@
+import ddf.minim.AudioInput;
+import ddf.minim.AudioOutput;
+import ddf.minim.AudioRecorder;
+import ddf.minim.Minim;
+import ddf.minim.ugens.FilePlayer;
+
+import java.io.InputStream;
+
+public class MinimSound {
+    AudioInput in;
+    AudioRecorder recorder;
+    boolean recorded;
+    //Playback
+    AudioOutput out;
+    FilePlayer player;
+    Minim minim;
+    //Record
+
+    public String sketchPath( String fileName ){
+        return Main.main.sketchPath(fileName);
+    }
+
+    public InputStream createInput(String fileName ){
+        return Main.main.createInput(fileName);
+    }
+    void minimSetup(){
+        minim = new Minim(this);
+
+        in = minim.getLineIn(Minim.STEREO);
+        recorder = minim.createRecorder(in,"Record1.wav");
+        out = minim.getLineOut(Minim.STEREO);
+
+    }
+    void newFile(String path){
+        recorder = minim.createRecorder(in,path);
+    }
+
+
+    void record(){
+        if(!recorder.isRecording()){
+            recorder.beginRecord();
+            System.out.println("Recording started");
+        }
+    }
+
+    void stoprecord(){
+        if(recorder.isRecording()){
+            recorder.endRecord();
+            System.out.println("Recording stopped");
+        }
+
+
+    }
+
+
+
+}
